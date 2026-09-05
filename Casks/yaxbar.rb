24e3982c28402628cf73e857e78f14f4932d15d1
Ashwani Agarwal <ashwani.a@outlook.com>
2026-09-05 18:43:27 +0100

    chore(cask): update SHA256 checksum for v2.2.2-beta release

--- Casks/yaxbar.rb
+++ Casks/yaxbar.rb
@@ -1,6 +1,6 @@
 cask "yaxbar" do
-  version "2.2.1-beta"
-  sha256 "82fd49a8038edc1a61b7da931c6d585e51d7016784f1189d1c33b27126bbda53"
+  version "2.2.2-beta"
+  sha256 "b55a61b2e6b6ab4c600d02d1f8e216eff20717224765aafe6ddad4d18699db2d"
 
   url "https://github.com/enVolt/yaxbar/releases/download/v#{version}/yaxbar.v#{version}.dmg"
   name "yaxbar"
